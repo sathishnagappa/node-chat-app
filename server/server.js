1df0253bf3da77f7613931638c1796aba08b82fa
sathish.nagappa <sathish.nagappa@brillio.com>
2018-04-12 20:52:20 +0530

    =socket io setup

--- server/server.js
+++ server/server.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const bodyParser = require("body-parser")
 const path = require("path");
+const socketIO = require("socket.io");
+const http = require("http");
 
 const port = process.env.PORT || 3000 ;
 const publicPath = path.join(__dirname, "../public");
 var app = express();
 
+var server = http.createServer(app);
+var io = socketIO(server);    
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+    console.log("New user connection");
+    socket.on("disconnect", () => {
+        console.log("User was Disconnected");
+       });
+})
+
+server.listen(port, () => {
  console.log("Server is up at " + port);
 });
